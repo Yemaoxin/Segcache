270551d26272f1b4a97d9e0f72860088490d5c8f
Yemaoxin <1648428830@qq.com>
2022-06-16 00:03:40 +0800

    基准测试复现

---
+++ cmake-build-debug/config.h
@@ -0,0 +1,25 @@
+#define VERSION_MAJOR 0
+#define VERSION_MINOR 1
+#define VERSION_PATCH 1
+
+#define VERSION_STRING "0.1.1"
+
+#define OS_PLATFORM OS_DARWIN
+
+/* #undef HAVE_ASSERT_LOG */
+
+/* #undef HAVE_ASSERT_PANIC */
+
+#define HAVE_BACKTRACE
+
+/* #undef HAVE_LOGGING */
+
+#define HAVE_STATS
+
+/* #undef HAVE_ITT_INSTRUMENTATION */
+
+#define TARGET_SLIMCACHE
+
+#define TARGET_TWEMCACHE
+
+/* #undef TARGET_REDIS */
